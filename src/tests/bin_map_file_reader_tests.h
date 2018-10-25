1cb89f8e87f33439f0e5eeae79a88a2a56de3b88
SebL <sebastien.levy@mentor.com>
2018-10-25 17:16:02 +0200

    Chapter 20 : Test suite REFACTO, flawed design
      Now the test suite use test doubles, but there is a hiccup.
      Dealing specifically with a file is a problem as it tends to create
      entities that are difficult to test.
    
      Some of the design of our initial solution is seriously flawed because
      of that.
      Fortunately, tools exist to help us in correct our design but it
      requires a little changes in our codebase.
    
      Following are the explanations :
      - files are files, pure I/O that are out of reach of unit testing
      - so far, we identified a file with its path
      - a possible fix could be brought by using streams instead of files.
      - streams have several forms and have a specific implementation for
        files.
      - string streams are completely usable in an unit test and have the
        exact same interface as file streams
    
      As a result, the REFACTO phase is not done yet. We have to TRANSFORM
      our code that must use streams instead of file paths.

--- src/tests/bin_map_file_reader_tests.h
+++ src/tests/bin_map_file_reader_tests.h
@@ -6,18 +6,20 @@
 #include "common/exceptions.h"
 #include "bin_map_file_reader.h"
 #include "test_doubles/valid_path_bin_map_file_reader.h"
+#include "test_doubles/bad_path_bin_map_file_reader.h"
+#include "test_doubles/empty_path_bin_map_file_reader.h"
 
 using Qx::BinMapping::BinMapFileReader;
 
 TEST(bin_map_file_reader, instantiate_with_empty_file_path)
 {
-    ASSERT_THROW( BinMapFileReader{ "" }, Qx::BinMapping::EmptyFilePath );
+    ASSERT_THROW( EmptyPathBinMapFileReader{ "" }, Qx::BinMapping::EmptyFilePath );
 }
 
 
 TEST(bin_map_file_reader, instantiate_with_bad_file_path)
 {
-    ASSERT_THROW( BinMapFileReader{ "foo" }, Qx::BinMapping::BadFilePath );
+    ASSERT_THROW( BadPathBinMapFileReader{ "foo" }, Qx::BinMapping::BadFilePath );
 }
 
 TEST(bin_map_file_reader, instantiate_with_valid_path)
