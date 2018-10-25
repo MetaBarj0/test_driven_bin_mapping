853137b36b777f59bc19927a6d388eecb7fa5894
SebL <sebastien.levy@mentor.com>
2018-10-25 16:43:50 +0200

    Chapter 18 : a new failling test
      testing the opening of a file with a valid path. It fails as it calls
      a non existing method
    
      Let's make it GREEN

--- src/tests/bin_map_file_reader_tests.h
+++ src/tests/bin_map_file_reader_tests.h
@@ -6,13 +6,22 @@
 #include "common/exceptions.h"
 #include "bin_map_file_reader.h"
 
+using Qx::BinMapping::BinMapFileReader;
+
 TEST(bin_map_file_reader, instantiate_with_empty_file_path)
 {
-    ASSERT_THROW( Qx::BinMapping::BinMapFileReader{ "" }, Qx::BinMapping::EmptyFilePath );
+    ASSERT_THROW( BinMapFileReader{ "" }, Qx::BinMapping::EmptyFilePath );
 }
 
 
 TEST(bin_map_file_reader, instantiate_with_bad_file_path)
 {
-    ASSERT_THROW( Qx::BinMapping::BinMapFileReader{ "foo" }, Qx::BinMapping::BadFilePath );
+    ASSERT_THROW( BinMapFileReader{ "foo" }, Qx::BinMapping::BadFilePath );
+}
+
+TEST(bin_map_file_reader, instantiate_with_valid_path)
+{
+    BinMapFileReader lReader{ "bar" };
+
+    ASSERT_TRUE( lReader.IsOpen() );
 }
