05ee90979c746056af7ed0a356e06404df6111df
SebL <sebastien.levy@mentor.com>
2018-10-25 16:16:05 +0200

    Chapter 16 - refacto on test suite
      The test suite has been repaired, taking in consideration the new
      signature of the class LvmWtBinMapStore. However, each test of this
      class is now dependant of the behavior of BinMapFileReader class as
      this is in the test we inject the correct dependency. Code smell
      spotted!
    
      In order to fix that and get LvmWtBinMapStore tests truly independent
      of BinmapFileReader behavior, we need to test double this latter using
      the interface we created juste before to decouple aforementionned
      production classes
    
      In summary, continuing REFACTO on test suite

--- src/tests/lvm_wt_tests.h
+++ src/tests/lvm_wt_tests.h
@@ -6,22 +6,30 @@
 #include "lvm_wt_bin_map_store.h"
 #include "lvm_wt_exceptions.h"
 #include "common/exceptions.h"
+#include "bin_map_file_reader.h"
 
 using namespace testing;
 
+using Qx::BinMapping::BinMapFileReader;
+using Qx::BinMapping::LvmWtBinMapStore;
+using Qx::BinMapping::EmptyFilePath;
+using Qx::BinMapping::BadFilePath;
+
 TEST(lvm_wt, instantiate_bin_map_store_with_empty_file_path)
 {
-    ASSERT_THROW( Qx::BinMapping::LvmWtBinMapStore{ "" }, Qx::BinMapping::EmptyFilePath );
+    ASSERT_THROW( LvmWtBinMapStore{ std::make_unique< BinMapFileReader >( "" ) },
+                  EmptyFilePath );
 }
 
 TEST(lvm_wt, instantiate_bin_map_store_with_invalid_file_path)
 {
-    ASSERT_THROW( Qx::BinMapping::LvmWtBinMapStore{ "foo" }, Qx::BinMapping::BadFilePath );
+    ASSERT_THROW( LvmWtBinMapStore{ std::make_unique< BinMapFileReader >( "foo" ) },
+                  BadFilePath );
 }
 
 TEST(lvm_wt, DISABLED_instantiate_bin_map_store_with_valid_file_path)
 {
-   Qx::BinMapping::LvmWtBinMapStore store{ "bar" };
+   LvmWtBinMapStore store{ std::make_unique< BinMapFileReader >( "bar" ) };
 
    /// TODO - activate this test asap, once unstuck
    /// ASSERT_FALSE( store.IsEmpty() );
