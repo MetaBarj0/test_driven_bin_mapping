0fc535d6542fda4324c0dff1c2b3a4e509d37dc1
SebL <sebastien.levy@mentor.com>
2018-10-26 18:02:50 +0200

    Chapter 34 - all GREEN
      case insensitive string is now fully tested.
    
      This time the implementation looks like quite less naive. I did not
      develop a full string class.
    
      Instead, I created a brand new trait class for character usable in the
      std::basic_string template.
    
      This is the recommended approach to build a case insensitive string
      type in c++
    
      The implementation uses some of new powers c++11 gave us like noexcept
      operator and std::declval in unevaluated context, template aliasing,
      and interesting type traits used in a SFINAE expression
    
      Anyway, we're GREEN and no REFACTO is needed
    
      Let's continue with a new string manipulation feature that is needed,
      check if a string start with something

--- src/common/string_manipulations.h
+++ src/common/string_manipulations.h
@@ -1,12 +1,63 @@
 #ifndef STRING_MANIPULATIONS_H
 #define STRING_MANIPULATIONS_H
 
-#include <string>
+#include <type_traits>
+#include <locale>
 
 namespace Qx
 {
 
-using CIString = std::string;
+
+template< typename CharType,
+          typename = std::enable_if_t< std::is_same< CharType, char >::value ||
+                                       std::is_same< CharType, wchar_t >::value ||
+                                       std::is_same< CharType, char16_t >::value ||
+                                       std::is_same< CharType, char32_t >::value > >
+struct ci_char_traits : public std::char_traits< CharType >
+{
+    static bool eq( CharType a, CharType b )
+    noexcept( noexcept( std::tolower( std::declval< CharType >(), std::declval< std::locale >() ) ) )
+    { return std::tolower( a, std::locale{} ) == std::tolower( b, std::locale{} ); }
+
+    static bool lt( CharType a, CharType b )
+    noexcept( noexcept( std::tolower( std::declval< CharType >(), std::declval< std::locale >() ) ) )
+    { return std::tolower( a, std::locale{} ) < std::tolower( b, std::locale{} ); }
+
+    static int compare( const CharType *s1, const CharType *s2, std::size_t n )
+    noexcept( noexcept ( lt( std::declval< CharType >(), std::declval< CharType >() ) ) )
+    {
+        while ( n-- != 0 )
+        {
+            if ( lt( *s1, *s2 ) ) return -1;
+            if ( lt( *s2, *s1 ) ) return 1;
+            ++s1; ++s2;
+        }
+
+        return 0;
+    }
+
+    static const CharType * find( const CharType *s, int n, CharType a )
+    noexcept( noexcept( std::tolower( std::declval< CharType >(), std::declval< std::locale >() ) ) )
+    {
+        const CharType l = std::tolower( a, std::locale{} );
+
+        while ( n-- != 0 )
+        {
+            if ( std::tolower( *s, std::locale{} ) == l )
+                return s;
+            s++;
+        }
+
+        return nullptr;
+    }
+};
+
+template< typename CharType,
+          typename CharTraits = ci_char_traits< CharType >,
+          typename Allocator = std::allocator< CharType > >
+using basic_ci_string = std::basic_string< CharType, CharTraits, Allocator >;
+
+using CIString = basic_ci_string< char >;
 
 }
 
--- src/tests/string_manipulations_tests.h
+++ src/tests/string_manipulations_tests.h
@@ -10,18 +10,14 @@ using Qx::CIString;
 
 TEST( case_insensitive_string, equal_strings_should_be_equivalent )
 {
-    std::string lNormalString{ "lowercase string" };
+    Qx::CIString lCaseInsensitiveString{ "lowercase string" };
 
-    CIString lInsensitiveString{ "lowercase string" };
-
-    ASSERT_TRUE( lNormalString == lInsensitiveString );
+    ASSERT_TRUE( lCaseInsensitiveString == "lowercase string" );
 }
 
 TEST( case_insensitive_string, case_different_strings_should_be_equivalent )
 {
-    std::string lNormalString{ "lowercase string" };
+    Qx::CIString lCaseInsensitiveString{ "lowercase string" };
 
-    CIString lInsensitiveString{ "LOWERCASE string" };
-
-    ASSERT_TRUE( lNormalString == lInsensitiveString );
+    ASSERT_TRUE( lCaseInsensitiveString == "LOWERCASE string" );
 }
