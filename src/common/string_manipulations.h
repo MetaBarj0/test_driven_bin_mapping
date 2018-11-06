d773db6d7a3cd075172107f1bc0f3bc980deff6d
SebL <sebastien.levy@mentor.com>
2018-11-06 10:10:59 +0100

    Chapter 57 : GREEN
      And it passes! Easy.
    
      OK, next test, real trim, no REFACTO needed so far
    
      Need a new RED test

--- src/common/string_manipulations.h
+++ src/common/string_manipulations.h
@@ -100,9 +100,8 @@ static String TrimStringLeftSideOf( const String &, CharTypeToTrim );
 template< typename CharType, typename CharTraits, typename Allocator >
 static auto TrimStringLeftSideOf( const std::basic_string< CharType, CharTraits, Allocator > &aString, CharType aChar )
 {
-    ( void )aString;
     ( void )aChar;
-    return std::basic_string< CharType, CharTraits, Allocator >{};
+    return aString;
 }
 
 }
