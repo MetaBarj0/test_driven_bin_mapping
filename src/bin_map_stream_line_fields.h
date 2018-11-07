0b03760cded745ac04b3655984ea01c79fc0589d
SebL <sebastien.levy@mentor.com>
2018-11-07 09:54:00 +0100

    Chapter 68 : GREEN but lack of discipline
      OK the previous RED test is now GREEN but...the production code I had
      to implement is quite heavy and it is difficult to make sure it is
      correct and/or reusable in other circumstances/tests.
    
      INFO : this is a common error about implementing too much production
      code for a feature.
    
      We can see a new class has been created but has not be covered by
      tests.
    
      Let's fix that in a REFACTO phase in which I'll create a test suite
      for the BinMapStreamLineFields class, with some passing tests.
    
      WARNING : despite its a good study case, this chapter shows an obvious
      lack of discipline leading to do TAD (test after development) instead
      of TDD.
    
      GREEN ==> REFACTO

--- src/bin_map_stream_line_fields.h
+++ src/bin_map_stream_line_fields.h
@@ -15,12 +15,47 @@ template< typename... FieldTypes >
 class BinMapStreamLineFields;
 
 template< typename FirstFieldType, typename... RemainingFieldTypes >
-class BinMapStreamLineFields< FirstFieldType, RemainingFieldTypes... >
+class BinMapStreamLineFields< FirstFieldType, RemainingFieldTypes... > :
+    public BinMapStreamLineFields< RemainingFieldTypes... >
 {
 public :
-    BinMapStreamLineFields( const StoreableBinMap &, const std::string & ) {}
+    BinMapStreamLineFields( const StoreableBinMap &aStore, const std::string &aLine ) :
+        BinMapStreamLineFields<RemainingFieldTypes...>{ aStore, GetLineForNextField( aStore, aLine ) },
+        mField{ {}, false }
+    {
+        const auto lDelimiter = aStore.GetFieldDelimiter();
+        auto lIterator = std::find_if( std::cbegin( aLine ),
+                                       std::cend( aLine ),
+                                       [ lDelimiter ]( char aChar ) { return aChar == lDelimiter; } );
 
-    bool IsEmpty() const noexcept { return true; }
+        if( lIterator != std::cend( aLine ) )
+        {
+            std::stringstream lStream;
+            lStream << std::string{ std::cbegin( aLine ), lIterator };
+            lStream >> mField.first;
+
+            mField.second = static_cast< bool >( lStream );
+        }
+
+    }
+
+    bool IsEmpty() const noexcept { return ! mField.second; }
+
+private :
+    auto GetLineForNextField( const StoreableBinMap &aStore, const std::string &aLine ) const
+    {
+        const auto lDelimiter = aStore.GetFieldDelimiter();
+        auto lIterator = std::find_if( std::cbegin( aLine ),
+                                       std::cend( aLine ),
+                                       [ lDelimiter ]( char aChar ) { return aChar == lDelimiter; } );
+        if( lIterator != std::cend( aLine ) )
+            ++lIterator;
+
+        return std::string{ lIterator, std::cend( aLine ) };
+    }
+
+private :
+    std::pair< FirstFieldType, bool > mField;
 };
 
 template< typename LastFieldType >
